e6900594d35711d32f19c1be26311480ec44c1d6
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-03-29 22:04:57 -0300

    running on ios and android

--- apps/native/.eslintrc.js
+++ apps/native/.eslintrc.js
@@ -1,4 +1,44 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true
+//     },
+//     "extends": [
+//         "plugin:react/recommended",
+//         "standard-with-typescript"
+//     ],
+//     "overrides": [
+//     ],
+//     "parserOptions": {
+//         "ecmaVersion": "latest",
+//         "sourceType": "module"
+//     },
+//     "plugins": [
+//         "react"
+//     ],
+//     "rules": {
+//     }
+// }
+
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ['*.json', '*.js'],
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
